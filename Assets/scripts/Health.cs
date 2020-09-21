d4b439ddd3f0b08ea0378e4d5f6c47e472206196
rzfvansuijdam <55277122+rzfvansuijdam@users.noreply.github.com>
2020-09-21 13:28:14 +0200

    Add project files.

---
+++ Assets/scripts/Health.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float health;
+    public float starthealth;
+    public bool alive = true;
+
+
+    public void Start()
+    {
+        alive = true;
+    }
+    public void TakeDamage(float value)
+    {
+
+        if (!alive)
+        {
+            return;
+        }
+
+        if (health <= 0)
+        {
+            health = 0;
+            alive = false;
+        }
+
+        health -= value;
+
+
+    }
+
+
+}
---
+++ Assets/scripts/moveto_waypoint.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#pragma warning disable 0219
+
+public class moveto_waypoint : MonoBehaviour
+{
+
+ 
+    public GameObject[] wayPoints;
+
+    public float spd;
+    public float distance;
+
+    public int currentPoint;
+
+    private Vector3 targetPosition;
+
+    void Start()
+    {
+     targetPosition = wayPoints[currentPoint].transform.position;
+     //Debug.Log("Current Position = " + currentPoint + " Distance = " + Vector3.Distance(transform.position, targetPosition));
+    }
+
+    void Update()
+    {
+        //Checks if the distance from the object to the way point is less than distance given
+        //Once true it will increment to the next point.
+        if ((Vector3.Distance(transform.position, targetPosition) < distance))
+        {
+            nextPoint();
+        }
+        else
+        {
+         float dir = 0;
+         //Moving and turning
+         this.transform.position = Vector3.MoveTowards(this.transform.position, targetPosition, 0.05F);
+        }
+    }
+
+    private void nextPoint()
+    {
+        //Checks to see if it reached the max waypoint possible
+        if (currentPoint >= wayPoints.Length - 1)
+        {
+            Debug.LogWarning("lolz im here");
+            //If true sets it to 0 so array doesn't go out of bounds.
+            currentPoint = 0;
+        }
+        else
+            currentPoint++;
+        //Sets the the targetPosition to the next waypoint.
+        targetPosition = wayPoints[currentPoint].transform.position;
+        Debug.Log("Current Position = " + currentPoint + " Distance = " + Vector3.Distance(transform.position, targetPosition));
+
+
+    }
+}
